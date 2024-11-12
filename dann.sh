6787bc07254eaaa87b4bbd202d7d0971318e8a33
twa251 <twa251@uky.edu>
2024-11-11 20:25:28 -0500

    Update dann.sh

--- dann.sh
+++ dann.sh
@@ -7,14 +7,14 @@
 #SBATCH --cpus-per-task=16
 #SBATCH --output=slurm-%A.%a.out
 #SBATCH --error=slurm-%A.%a.err
-#SBATCH --mail-user=zeya.wang@uky.edu 
+#SBATCH --mail-user=twa251@uky.edu 
 #SBATCH --mail-type=ALL
 
 module load cuda/12.1
 conda info --envs
 eval $(conda shell.bash hook)
-source ~/miniconda/etc/profile.d/conda.sh
-conda activate myenv
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate pylava10
 
-python3 /home/zwa281/DANN/dann_office.py --source amazon --target webcam --lr 0.01 --beta 0.1
+python3 /home/twa251/DANN/dann_office.py --source amazon --target webcam --lr 0.01 --beta 0.1
 
