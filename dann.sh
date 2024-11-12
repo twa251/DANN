6f88598ddbeab8ee9368f7afd777ed7a75a0e011
twa251 <twa251@uky.edu>
2024-11-11 20:23:40 -0500

    Update dann.sh

--- dann.sh
+++ dann.sh
@@ -2,19 +2,19 @@
 
 #SBATCH --partition=wang
 #SBATCH --gpus-per-node=rtxa5500:8
-#SBATCH --nodes=1 --mem=128G --time=168:00:00
+#SBATCH --nodes=1 --mem=64G --time=168:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
 #SBATCH --output=slurm-%A.%a.out
 #SBATCH --error=slurm-%A.%a.err
-#SBATCH --mail-user=zeya.wang@uky.edu 
-#SBATCH --mail-type=ALL
+#SBATCH --mail-user=twa251@uky.edu 
+#SBATCH --mail-type=end
 
-module load cuda/12.1
+module load cuda/12.5
 conda info --envs
 eval $(conda shell.bash hook)
-source ~/miniconda/etc/profile.d/conda.sh
-conda activate myenv
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate pylava10
 
-python3 /home/zwa281/DANN/dann_office.py --source amazon --target webcam --lr 0.01 --beta 0.1
+python3 /home/twa251/DANN/dann_office.py --source amazon --target webcam --lr 0.01 --beta 0.1
 
